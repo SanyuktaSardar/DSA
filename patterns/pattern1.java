f23d2e97f88a28c2d7aa57a6d11c6ea569b19507
Sanyukta Sardar <ssanyukta16@gmail.com>
2025-08-16 21:12:49 +0530

    Add files via upload

---
+++ patterns/pattern1.java
@@ -0,0 +1,18 @@
+class pattern1{
+    public static void main(String args[])
+    {
+        int n = 4;
+        int m = 5;
+
+        //outer loop
+        for (int i=1; i<= n;i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                //inner loop
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
