298dc0d3b058571fc4b4686bdba4a4b3638ecfbd
Sanyukta Sardar <ssanyukta16@gmail.com>
2025-08-21 01:05:10 +0530

    Add files via upload

---
+++ OPEARATORS/Operations1.java
@@ -0,0 +1,30 @@
+public class Operations1{
+    public static void main(String[] args) {
+        //Symbols that tells compiler to perform some operation
+        //binary operation
+        //Arithmetic Operators(+,-,*,/,%,++,--)
+        //preIncrement
+        int a=10;
+        int b = 0;
+        b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        //postIncrement
+        int x = 10;
+        int y =0;
+        y = x++;
+        System.out.println(x);
+        System.out.println(y);
+
+        //relational Operators(==,!=,>,<,<=,>=) True/False
+        //Logical Operators(&&,||,!)
+        //memory store in hexadecimal
+
+        //Bitwise operation(&,|,^(XOR),~,<<,>>)
+        //XOR-> similar case return 0 or False
+
+        //Assignment operators(=,+=,-=,*=,/=)
+
+    }
+}
